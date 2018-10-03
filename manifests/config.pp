8db4e89299e4d42314eb6f479ba646f5d19aa6d8
BChristianV <bchristianv@icloud.com>
2018-10-02 19:14:21 -0700

    Manage repo config directory; Comment clarification and cleanup.

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,41 +5,39 @@
 # @param [Stdlib::Absolutepath] cachedir
 #   Directory where yum should store its cache and db files. Default value: /var/cache/yum/$basearch/$releasever.
 #
-# @param [Boolean] keepcache
+# @param [Enum['0', '1']] keepcache
 #   Determines whether or not yum keeps the cache of headers and packages after successful installation.
 #   Default value: 1 (keep files).
 #
-# @param [Integer] debuglevel
+# @param [Enum['0', '1', '2', '3', '4', '5', '6', '7', '8','9', '10']] debuglevel
 #   Debug message output level. Practical range is 0-10. Default value: 2.
 #
 # @param [Stdlib::Absolutepath] logfile
 #   Full directory and file name for where yum should write its log file. Default value: /var/log/yum.log.
 #
-# @param [Boolean] exactarch
-#   ???. Default value: 1.
+# @param [Enum['0', '1']] exactarch
+#   Only update the architectures of packages that are installed. Updates must precisely preserve the architecture.
+#   Default value: 1.
 #
-# @param [Boolean] obsoletes
+# @param [Enum['0', '1']] obsoletes
 #   Enables yum's obsoletes processing logic. Useful when doing distribution level upgrades. Default value: 1.
 #
-# @param [Boolean] gpgcheck
+# @param [Enum['0', '1']] gpgcheck
 #   Tells yum whether or not it should perform a GPG signature check on packages. Default value: 1.
 #
-# @param [Boolean] plugins
+# @param [Enum['0', '1']] plugins
 #   Global switch to enable or disable yum plugins. Default value: 1.
 #
 # @param [Integer] installonly_limit
 #   Number of packages listed in installonlypkgs to keep installed at the same time.
-#   Default value: 5 (on CentOS; RedHat default value: 3).
-#
-# @param [Stdlib::Httpurl] bugtracker_url
-#   URL where bugs should be filed for yum. Default value: null.
+#   Default value: 5 (on CentOS; Red Hat default value: 3).
 #
 # @param [Stdlib::Httpurl] bugtracker_url
 #   URL where bugs should be filed for yum. Default value: null (distro-specific).
 #
 # @param [String] distroverpkg
 #   The package used by yum to determine the "version" of the distribution.
-#   Default value: null (CentOS default value: centos-release).
+#   Default value: centos-release (on CentOS; Red Hat default value: null).
 #
 # @param [Hash] extra_options
 #   Other yum configuration options to set. See yum_config-extra_options.example in this module's data directory.
@@ -66,6 +64,13 @@ class yum::config (
     mode    => '0644',
     owner   => '0',
     content => template('yum/yum.conf.erb'),
+  }
+
+  file { '/etc/yum.repos.d':
+    ensure   => directory,
+    group    => '0',
+    mode     => '0755',
+    owner    => '0',
   }
 
 }
